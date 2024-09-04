765a653c7000910c4dd1eb11b0c88a7f79c9016f
ArmandtheMage <armandthemagic@gmail.com>
2024-09-04 09:39:43 +0200

    Rinominato alcuni file in modo più consono

---
+++ my_card.py
@@ -0,0 +1,108 @@
+class Card:
+    def __init__(self, value) -> None:
+        self.value = value
+
+    def __str__(self):
+        if self.suit == '*':
+            return self.value
+        else:
+            return str(self.value)
+        
+    def __eq__(self, obj: object) -> bool:
+        if self.value == obj.value:
+            return True
+        else:
+            return False
+    
+    # ! da valutare se va bene o se in futuro potrebbe essere utile
+    # ! settato ad uno per le addizioni nei set
+    def __len__(self):
+        return 1
+    
+
+class FrenchCard(Card):
+    """
+    Carte Francesi, comunemente utilizzate per poker, burraco, etc
+    """
+    SUITS_REFERENCE = ['Cuori', 'Quadri', 'Fiori', 'Picche']
+    SUITS_UNICODE_SIMBOL = [chr(0x2665), chr(0x2666), chr(0x2663), chr(0x2660)]
+    # Non necessario nella nuova versione
+    SUITS = {}
+    for elem in zip(SUITS_REFERENCE, SUITS_UNICODE_SIMBOL):
+        SUITS.update({elem[0] : elem[1]})
+
+    CARD_VALUES = list(range(2, 11)) + ['J', 'Q', 'K', 'A']
+    
+    # ! Jolly hanno valore -1
+    WILD_CARD_VALUE = "Jolly"
+    WILD_CARD_SUIT = '*'
+        
+    def __init__(self, value, suit) -> None:
+        super().__init__(value)
+        # ? gestire la creazione da numero o char invece che solo numero
+        self.suit = suit
+        if self.value < 0:
+            self.name = self.WILD_CARD_VALUE
+        else:
+            self.name = str(self.CARD_VALUES[self.value]) +\
+                    " di " + self.SUITS_REFERENCE[self.suit]
+        
+    def __str__(self):
+        if self.value < 0:
+            return self.WILD_CARD_VALUE
+        else:
+            return str(self.CARD_VALUES[self.value]) + self.SUITS_UNICODE_SIMBOL[self.suit]
+        
+    def __eq__(self, obj: object) -> bool:
+        return super().__eq__(obj) and self.suit == obj.suit
+    
+
+    def clear(self): # ! check if is good
+        self = None
+
+class Hieroglyp_Khepri(Card):
+    """
+    Carte geroglifico per il gioco Khepri.
+    Sono caratterizzate da un colore, un simbolo e un valore di risorsa.
+    """
+    SIMBOLS = ["Occhio", "Sciacallo", "Falco", "Scarabeo", "Gatto", "Anubi", "Ankh"]
+    COLORS = ["Rosso", "Blu", "Giallo", "Verde", "Marrone"]
+
+    def __init__(self, value, simbol, color) -> None:
+        super().__init__(value) # resource value
+        self.simbol = simbol
+        self.color = color
+
+    def __str__(self):
+        return self.SIMBOLS[self.simbol] + " " + self.COLORS[self.color]
+    
+    def __eq__(self, obj: object) -> bool:
+        return self.simbol == obj.simbol and self.color == obj.color
+
+
+class Scribe_Khepri(Card):
+    """
+    Carte scriba per il gioco Khepri.
+    Sono caratterizzate da una lista di colori chiamate 'competenze',
+    una lista di simboli chiamata 'sequenza', un potere e opzionale
+    un simbolo di riferimento
+    """
+    def __init__(self, value, competence:list[str], sequence:list) -> None:
+        super().__init__(value) # value = power
+        self.competence = competence
+        self.sequence = sequence
+        
+        
+
+if __name__ == "__main__":
+    card = FrenchCard(2, 2)
+    c1 = FrenchCard(2, 2)
+    c2 = FrenchCard(-1, '*')
+    print(card)
+    print(c1)
+    print(c1 == card)
+    print(c1 == c2)
+    #c1 = CardKhepri(2, 'P', 4)
+    #c2 = CardKhepri(3, 'P', 4)
+    print(len(FrenchCard.SUITS))
+
---
+++ my_deck.py
@@ -0,0 +1,169 @@
+import random
+import my_card
+import sets_card as sets
+
+SEEDS = ['C', 'Q', 'F', 'P']
+CARD_VALUES = list(range(2, 11))
+CARD_VALUES.extend(['J', 'Q', 'K', 'A'])
+
+
+class Deck:
+    """
+    Definisce un mazzo. Il mazzo ha una pila di pesca, una pila degli
+    scarti, e definisce.
+    """
+    # ? capire se game deve esserci oppure creare un erede per gioco
+    def __init__(self, game = None) -> None:
+        self.pile = sets.CardStack([])
+        self.graveyard = sets.CardStack([])
+
+    def __str__(self) -> str:
+        return str(self.pile) + "~~~" + str(self.graveyard)
+    
+
+    def shuffle(self, full = False):
+        """
+        Mescola il mazzo.
+        full
+            booleano che indica se bisogna mescolare anche gli scarti.
+            default a 'False'
+        """
+        if full:
+            self.pile += self.graveyard # ricontrollare se da problemi
+            self.graveyard.clear()
+        random.shuffle(self.pile)
+        return self
+
+    # TODO Modifica il metodo togliendo il default
+    def draw(self, number = 1, shuffle_when_empty=False):
+        """
+        Funzione che pesca carte dalla cima del mazzo.
+        number
+            numero di carte da pescare . defaul 1
+        shuffle_when_empty
+            booleano che indica se in caso il mazzo sia vuoto rimescola
+            gli scarti formando una nuova pila degli scarti e continua
+            a pescare.
+        """
+        if shuffle_when_empty and (cards_left := len(self.pile) - number) < 0:
+            card_drawed = self.pile.take(abs(cards_left))
+            self.shuffle(full=True)
+            card_drawed += self.pile.take(number + cards_left)
+            return card_drawed
+        else:
+            return self.pile.take(number)
+    
+    def add_graveyard(self, cards: sets.Set_of_Cards | my_card.Card):
+        """
+        Aggiunge la carta o il set di carte alla pila degli scarti.
+        """
+        self.graveyard += cards
+        cards.clear()
+    
+
+class PokerDeck(Deck):
+    def __init__(self, game = None) -> None:
+        super().__init__(game)
+        for value in range(len(my_card.FrenchCard.CARD_VALUES)):
+            for suit in range(len(my_card.FrenchCard.SUITS)):
+                self.pile += my_card.FrenchCard(value, suit)
+
+
+class Deck1:  # non buttare draw
+    def __init__(self, card_type:str="Francese"):
+        # ? gestirlo in modo differente ?
+        if card_type == "Francese":
+            self.pile = []
+            for seed in SEEDS:
+                for value in CARD_VALUES:
+                    card = my_card.Card(value, seed)
+                    self.pile.append(card)
+            self.pile.append(my_card.Card("Jolly", '*'))
+            self.pile.append(my_card.Card("Jolly", '*'))
+        else:
+            self.pile = []
+            print(f"{card_type} not supported")
+        self.graveyard = []
+
+    def __str__(self):
+        string = ""
+        if len(self.pile) > 0:
+            for card in self.pile:
+                string += f"{card}, "
+            
+            string = string[::-1].replace(',', '~' * 3, 1)
+            string = string[::-1]
+        else:
+            string += '~' * 3
+
+        if len(self.graveyard) > 0:
+            string = string[:-1]
+            for card in self.graveyard:    
+                string += f"{card}, "
+                slice_index = -2
+        elif string == "~~~":
+            slice_index = None
+        else:
+            return string[:-1]
+        
+        return string[:slice_index]
+
+    def show(self):
+        for i, card in enumerate(self.pile):
+            print(f"La carta {i + 1}-esima è: {card}")
+        for i, card in enumerate(self.graveyard):
+            print(f"La {i + 1}-esima carta degli scarti è: {card}")
+
+    def shuffle(self, full=False):
+        if not full:
+            self.pile.extend(self.graveyard)
+            self.graveyard = []
+
+        random.shuffle(self.pile)
+        return self
+
+    def draw(self, n:int, shuffle_when_empty=False,
+             jolly_redraw=False) -> list:
+        #TODO inserire controllo se si vuole pescare più del mazzo
+        if n > 0:
+            cards = []
+            for i in range(n):
+                try:
+                    cards.append(self.pile.pop(0))
+                except IndexError as e:
+                    if shuffle_when_empty:
+                        self.shuffle(full=True)
+                        cards.append(self.pile.pop(0))
+                    else:
+                        print("Carte Finite")
+
+                if str(cards[-1]) == "Jolly" and jolly_redraw:
+                    print("Estratto un Jolly pesco una carta extra!")
+                    cards.append(self.pile.pop(0))
+            return cards
+        else:
+            print("Seleziona un numero positivo!")
+        
+    def add_graveyard(self, cards:object | list):
+        if cards.__class__ == my_card:
+            cards = [cards]
+        for card in cards:
+            self.graveyard.insert(0, card)
+            #print(f"Scartato {card}")
+
+
+
+if __name__ == "__main__":
+    prova = PokerDeck()
+    print(prova)
+    prova.shuffle()
+    t = prova.draw(20)
+    prova.add_graveyard(t)
+    print(prova)
+    t = prova.draw(20)
+    prova.add_graveyard(t)
+    print(prova)
+    t = prova.shuffle().draw(20, True)
+    print(f"\n\n{t}")
+    #prova.add_graveyard(t)
+    print(prova)
